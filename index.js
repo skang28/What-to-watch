203f87af882f69ffcb4b91c45738d6061c2612a7
Steven Kang <skang28@gmail.com>
2019-08-22 20:01:43 -0400

    updated files

--- index.js
+++ index.js
@@ -1,48 +1,155 @@
-function getAmazonResults() {
+const mbdApiKey = 'c925ed2e2ec6bdab063211233d710e91';
+const omdbApiKey = '4b3bd4be';
+const maxResults = 3;
 
+
+
+function getGenres(callback) {
+    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=c925ed2e2ec6bdab063211233d710e91&language=en-US')
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+        console.log(responseJson)
+        callback(responseJson)
+    })
+    .catch(err => {
+        $('.errorMessage').text(`Uh oh. Something went wrong: ${err.message}`)
+    });
 }
 
-function getEbayResults() {
-    const apiID = 'StevenKa-ProductS-PRD-c447dc556-7d1b6d38';
-    fetch('https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=StevenKa-ProductS-PRD-c447dc556-7d1b6d38&RESPONSE-DATA-FORMAT=json&REST-PAYLOAD&keywords=harry%20potter%20phoenix&paginationInput.entriesPerPage=2')
+function getMovieResults(genreList) {
+    console.log(genreList);
+    // can't find genre
+    let genreID = genreList.find(genre => genre.name === selectGenre).id;
+    console.log(genreID);
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${mdbApiKey}&language=en-US&include_adult=true&with_genres=${genreID}&include_video=false`)
     .then(response => {
         if(response.ok) {
             return response.json();
         }
         throw new Error(response.statusText); 
     })
-    .then(responseJson => displayEbayResults(responseJson))
+    .then(responseJson => {
+        displayMovieResults(responseJson);
+        getImdbResults(responseJson);
+    })
     .catch(err => {
         $('.errorMessage').text(`Uh oh. Something went wrong: ${err.message}`)
     });
 }
 
-function getWalmartResults() {
-
+function getImdbResults(movieTitle) {
+    // need to extract movietitle from movieresults and add it here
+    fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=${omdbApiKey}`)
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText); 
+    })
+    .then(responseJson => {
+        displayMovieResults(responseJson)
+    })
+    .catch(err => {
+        $('.errorMessage').text(`Uh oh. Something went wrong: ${err.message}`)
+    });
 }
 
-function displayAmazonResults() {
+// movies[Math.random()*(movies.length-1))]
 
+function getTVResults(genreList) {
+    let genreID = genreList.find(genre => genre.genres.name === $('option').val().id);
+    fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${mdbApiKey}&language=en-US&with_genres=${genreID}`)
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText); 
+    })
+    .then(responseJson => displayTVResults(responseJson))
+    .catch(err => {
+        $('.errorMessage').text(`Uh oh. Something went wrong: ${err.message}`)
+    });  
 }
 
-function displayEbayResults(responseJson) {
-    console.log(responseJson);
-    $('.ebay').empty();
-
-    $('.ebay').append(`
-    <p>${responseJson.findItemsByKeywordsReponse.searchResult.item.title}</p>`);
-
+function displayMovieResults(responseJson) {
+    $('main').html(`
+        <div class="poster"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2${responseJson.results.poster_path}" alt="Poster of movie">
+        <h2>${responseJson.results.title}</h2>
+        <h2>${responseJson.results.release_date}</h2>
+        <p>${responseJson.results.overview}</p>
+        <div class="castInfo">
+        <div class="imdbContainer">
+        <div class="rottenTomatoescontainer">
+        `)
 }
 
-function displayWalmartResults() {
+function displayTVResults() {
+    $('main').html(`
+        <div class="poster"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2${responseJson.results.poster_path}" alt="Poster of movie">
+        <h2>${responseJson.results.title}</h2>
+        <h2>${responseJson.results.release_date}</h2>
+        <p>${responseJson.results.overview}</p>
+        <div class="castInfo">
+        <div class="imdbContainer">
+        <div class="rottenTomatoescontainer">
+        `)
+}
 
+function searchPage() {
+    $('header').html(`
+        <h1>Welcome Message!</h1>`);
+    $('main').html(`
+        <form class='mainForm'>
+            <fieldset>
+                <label>Select Genre</label>
+                <select id='genreList'>
+                    <option value='Action'>Action</option>
+                    <option value='Adventure'>Adventure</option>
+                    <option value='Animation'>Animation</option>
+                    <option value='Comedy'>Comedy</option>
+                    <option value='Crime'>Crime</option>
+                    <option value='Documentary'>Documentary</option>
+                    <option value='Drama'>Drama</option>
+                    <option value='Family'>Family</option>
+                    <option value='Fantasy'>Fantasy</option>
+                    <option value='History'>History</option>
+                    <option value='Horror'>Horror</option>
+                    <option value='Music'>Music</option>
+                    <option value='Mystery'>Mystery</option>
+                    <option value='Romance'>Romance</option>
+                    <option value='Science Fiction'>Science Fiction</option>
+                    <option value='TV Movie'>TV Movie</option>
+                    <option value='Thriller'>Thriller</option>
+                    <option value='War'>War</option>
+                    <option value='Western'>Western</option>
+                </select>
+                <br><br>
+                <label class='selectMovieOrTV'><input type='radio' value='Movie' required>Movie</label>
+                <label class='selectMovieOrTV'><input type='radio' value='TV Show' required>TV Show</label>
+                <br><br>
+                <input type="submit" value="What should I watch?">
+            </fieldset>
+        </form>`);
 }
 
 function watchForm() {
     $('form').submit(event => {
-        event.preventDefault();
-        getEbayResults();
+        event.preventDefault();    
+        let selectType = $('input:checked').val();  
+        if (selectType === 'TV Show') {
+            getGenres(getTVResults);
+        }
+        else {
+            getGenres(getMovieResults);
+        }
     });
 }
 
+$(searchPage);
 $(watchForm);
+
