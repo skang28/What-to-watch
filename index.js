bb6ba5f87133b616795b2da4a354e52959895c54
Steven Kang <skang28@gmail.com>
2019-08-31 19:33:17 -0400

    mvp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mbdApiKey = 'c925ed2e2ec6bdab063211233d710e91';
 const omdbApiKey = '4b3bd4be';
-const maxResults = 3;
+const genreStore = {};
 
 
 
@@ -23,10 +23,9 @@ function getGenres(callback) {
 
 function getMovieResults(genreList) {
     console.log(genreList);
-    // can't find genre
-    let genreID = genreList.find(genre => genre.name === selectGenre).id;
+    let genreID = genreList.genres.find(genre => genre.name === genreStore.selectGenre).id;
     console.log(genreID);
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${mdbApiKey}&language=en-US&include_adult=true&with_genres=${genreID}&include_video=false`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${mbdApiKey}&language=en-US&include_adult=true&with_genres=${genreID}&include_video=false`)
     .then(response => {
         if(response.ok) {
             return response.json();
@@ -34,17 +33,29 @@ function getMovieResults(genreList) {
         throw new Error(response.statusText); 
     })
     .then(responseJson => {
-        displayMovieResults(responseJson);
-        getImdbResults(responseJson);
+       getImdbResults(processResults(responseJson));
     })
     .catch(err => {
         $('.errorMessage').text(`Uh oh. Something went wrong: ${err.message}`)
     });
 }
 
-function getImdbResults(movieTitle) {
+function processResults(object) {
+    let random_object = object.results[Math.floor(Math.random()*(object.results.length-1))];
+    if(random_object.first_air_date) {
+        random_object.release_date=random_object.first_air_date;
+    }
+    if(random_object.name) {
+        random_object.title=random_object.name;
+    }
+    return random_object;
+}
+
+function getImdbResults(movieObjects) {
     // need to extract movietitle from movieresults and add it here
-    fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=${omdbApiKey}`)
+    // use encodeURI component
+    console.log(movieObjects);
+    fetch(`http://www.omdbapi.com/?t=${encodeURIComponent(movieObjects.title)}&apikey=${omdbApiKey}`)
     .then(response => {
         if(response.ok) {
             return response.json();
@@ -52,94 +63,55 @@ function getImdbResults(movieTitle) {
         throw new Error(response.statusText); 
     })
     .then(responseJson => {
-        displayMovieResults(responseJson)
+        displayResults(movieObjects,responseJson)
     })
     .catch(err => {
         $('.errorMessage').text(`Uh oh. Something went wrong: ${err.message}`)
     });
 }
-
-// movies[Math.random()*(movies.length-1))]
 
 function getTVResults(genreList) {
-    let genreID = genreList.find(genre => genre.genres.name === $('option').val().id);
-    fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${mdbApiKey}&language=en-US&with_genres=${genreID}`)
+    let genreID = genreList.genres.find(genre => genre.name === genreStore.selectGenre).id;    
+    fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${mbdApiKey}&language=en-US&with_genres=${genreID}`)
     .then(response => {
         if(response.ok) {
             return response.json();
         }
         throw new Error(response.statusText); 
     })
-    .then(responseJson => displayTVResults(responseJson))
-    .catch(err => {
+    .then(responseJson => {
+        getImdbResults(processResults(responseJson));
+     })    
+     .catch(err => {
         $('.errorMessage').text(`Uh oh. Something went wrong: ${err.message}`)
     });  
 }
 
-function displayMovieResults(responseJson) {
+function displayResults(movieObjects,imdbResults) {
+    console.log(imdbResults);
+    let rottenTitle = movieObjects.title.replace(new RegExp(/ |-/, 'g'), '_').replace(new RegExp(/:|!|\/|\.|/,'g'),'').replace(new RegExp(/&/,'g'),'and').replace(new RegExp(/_+/,'g'),'_').toLowerCase();
+    console.log(rottenTitle);
     $('main').html(`
-        <div class="poster"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2${responseJson.results.poster_path}" alt="Poster of movie">
-        <h2>${responseJson.results.title}</h2>
-        <h2>${responseJson.results.release_date}</h2>
-        <p>${responseJson.results.overview}</p>
-        <div class="castInfo">
+        <div class="poster"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2${movieObjects.poster_path}" alt="Poster of movie"></div>
+        <h2>${movieObjects.title}</h2>
+        <h2>${movieObjects.release_date}</h2>
+        <p>${movieObjects.overview}</p>
+        <div class="castInfo">${imdbResults.Actors}</div>
         <div class="imdbContainer">
+            <p>${imdbResults.imdbRating}</p>
+            <a href="http://www.imdb.com/title/${imdbResults.imdbID}" target="_blank">IMDb</a>
+        </div>
         <div class="rottenTomatoescontainer">
+            <a href="http:www.rottentomatoes.com/m/${rottenTitle}" target="_blank">RottenTomatoes</a>
+        </div>
         `)
-}
-
-function displayTVResults() {
-    $('main').html(`
-        <div class="poster"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2${responseJson.results.poster_path}" alt="Poster of movie">
-        <h2>${responseJson.results.title}</h2>
-        <h2>${responseJson.results.release_date}</h2>
-        <p>${responseJson.results.overview}</p>
-        <div class="castInfo">
-        <div class="imdbContainer">
-        <div class="rottenTomatoescontainer">
-        `)
-}
-
-function searchPage() {
-    $('header').html(`
-        <h1>Welcome Message!</h1>`);
-    $('main').html(`
-        <form class='mainForm'>
-            <fieldset>
-                <label>Select Genre</label>
-                <select id='genreList'>
-                    <option value='Action'>Action</option>
-                    <option value='Adventure'>Adventure</option>
-                    <option value='Animation'>Animation</option>
-                    <option value='Comedy'>Comedy</option>
-                    <option value='Crime'>Crime</option>
-                    <option value='Documentary'>Documentary</option>
-                    <option value='Drama'>Drama</option>
-                    <option value='Family'>Family</option>
-                    <option value='Fantasy'>Fantasy</option>
-                    <option value='History'>History</option>
-                    <option value='Horror'>Horror</option>
-                    <option value='Music'>Music</option>
-                    <option value='Mystery'>Mystery</option>
-                    <option value='Romance'>Romance</option>
-                    <option value='Science Fiction'>Science Fiction</option>
-                    <option value='TV Movie'>TV Movie</option>
-                    <option value='Thriller'>Thriller</option>
-                    <option value='War'>War</option>
-                    <option value='Western'>Western</option>
-                </select>
-                <br><br>
-                <label class='selectMovieOrTV'><input type='radio' value='Movie' required>Movie</label>
-                <label class='selectMovieOrTV'><input type='radio' value='TV Show' required>TV Show</label>
-                <br><br>
-                <input type="submit" value="What should I watch?">
-            </fieldset>
-        </form>`);
 }
 
 function watchForm() {
     $('form').submit(event => {
-        event.preventDefault();    
+        event.preventDefault(); 
+        let selectGenre = $('select').val();
+        genreStore.selectGenre = selectGenre;   
         let selectType = $('input:checked').val();  
         if (selectType === 'TV Show') {
             getGenres(getTVResults);
@@ -150,6 +122,4 @@ function watchForm() {
     });
 }
 
-$(searchPage);
 $(watchForm);
-
